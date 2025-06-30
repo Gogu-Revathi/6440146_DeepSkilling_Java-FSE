856e651e3a0dacae40930e304b0b0830a99358e5
Gogu-Revathi <gogurevathi9@gmail.com>
2025-06-30 12:54:42 +0530

    Delete WEEK-2-PLSQL directory

--- WEEK-2-PLSQL/HandsOn1_Control Structures/Scenario3/scenario3.sql
+++
@@ -1,8 +0,0 @@
-BEGIN
-  FOR l IN (SELECT LoanID, CustomerID, EndDate
-            FROM Loans
-            WHERE EndDate BETWEEN SYSDATE AND SYSDATE + 30) LOOP
-    DBMS_OUTPUT.PUT_LINE('Reminder: Customer ' || l.CustomerID || ' has a loan due on ' || TO_CHAR(l.EndDate, 'YYYY-MM-DD'));
-  END LOOP;
-END;
-/
